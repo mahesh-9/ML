7e95e39600cbae9d21a09d67dee553f49beb33f3
aakash5897 <aakash.dokka@gmail.com>
2017-05-25 23:54:04 +0530

    added linear and logistic regr

---
+++ ml/lr.py
@@ -0,0 +1,54 @@
+""" Linear regression for multiple features """
+__author__="Aakash"
+#import numpy as np
+from .numc import *
+class LR():
+	#def __init__(self):
+		#self.get_data()
+		#self.count=0
+	def fit(self,X,Y):
+		if not isinstance(X,np.ndarray):self.a=np.array(X)
+		else:self.a=X
+		inst=nx(self.a)
+		self.feat=inst.add_col(col_no=0,val=1)
+		self.target=Y
+		self.theta=np.zeros(self.feat.shape[1])
+		self.m=self.a.shape[0]
+	def hyp(self,it=None):
+		"""returns the hypothesis equation of a pirticular feature"""
+		return self.theta.dot(self.feat[it])				
+	def cost(self):
+		"""computes the cost"""
+		#self.count+=1
+		#self.m=self.a.shape[0]
+		sum=0
+		for i in range(self.m):
+			res=(self.hyp(it=i)-self.target[i])**2
+			sum+=res
+		return (1/2*(self.m))*(sum)	
+	def gd(self,rate=0.001,loops=700):
+		"""gradient descent"""
+		for k in range(loops):
+			for i in range(len(self.theta)):
+				ts=0
+				for j in range(self.m):
+					res=(self.hyp(it=j)-self.target[j])*self.feat[j][i]
+					ts+=res
+				self.theta[i]-=rate*(1/(self.m))*(ts)
+				#self.theta[i]-=rate*(ts)
+			t=self.cost()
+		#return self.theta
+	def predict(self,x):
+		"""predicting the values"""
+		self.gd()
+		#return x.dot(self.theta[1:])
+		return x.dot(self.theta[1:])+self.theta[0]
+	@property
+	def intercept_coef(self):
+		return "intercept:{} coef:{}".format(self.theta[0],self.theta[1:])
+		
+
+				
+			
+		
+		
---
+++ ml/numc.py
@@ -0,0 +1,31 @@
+import numpy as np
+class nx:
+	"""a class for custom methods in numpy"""
+	def __init__(self,ob):
+		if not isinstance(ob,np.ndarray):
+			raise("expected np.array object")
+		else:
+			self.ob=ob
+	def add_col(self,col_no,val=np.inf):
+		"""this method returns an numpy.ndarray
+		which consists of an extra column (with a same value) to the original array object passed in __init__.
+		col_no=column number to add
+		val=value to be passed""" 
+		new_shape=self.ob.shape[0],self.ob.shape[1]+1
+		b=np.full(new_shape,np.inf)
+		for i in range(len(self.ob)):
+			temp=np.insert(self.ob[i],col_no,val)
+			b[i]=temp
+		return b
+	def zigzag(self,row_no):
+		if self.ob[row_no]==self.ob[-1]:
+			print("not possible!!!")
+		else:
+			x=np.full(self.ob.shape[1],np.inf)
+			for i in range(len(x)):
+				for j in range(len(x)):
+					if j%2!=0:continue
+					else:pass
+			
+			
+		
