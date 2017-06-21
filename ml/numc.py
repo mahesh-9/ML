2653725c2249bb41d49700472852559a53dae35c
Aakash Dm <aakash.dokka@gmail.com>
2017-06-21 23:55:24 +0530

    Update numc.py

--- ml/numc.py
+++ ml/numc.py
@@ -17,6 +17,32 @@ class nx:
 			temp=np.insert(self.ob[i],col_no,val)
 			b[i]=temp
 		return b
-	
-			
-		
+				
+def split_train_test(X,Y,per=20):
+		class_set=set(Y)
+		l=list(Y)
+		print("l",l)
+		count=l.count(list(class_set)[0])
+		print("coutn",count)
+		dim_1=int((per/100)*count)*len(list(class_set))
+		dim_2=X.shape[1]
+		print(dim_1,dim_2)
+		n_train_f=np.full([dim_1,dim_2],np.inf)
+		n_test_f=np.full([len(X)-dim_1,dim_2],np.inf)
+		n_train_t=np.full([dim_1,],np.inf)
+		n_test_t=np.full([len(X)-dim_1,],np.inf)	
+		f_i=0
+		ins=f_i
+		s_i=int((20/100)*count)
+		ind=s_i
+		for i in range(len(list(class_set))):
+			n_train_f[ins:ins+ind]=X[f_i:s_i]
+			n_test_f[s_i:f_i+50]=X[s_i:f_i+50]
+			n_train_t[f_i:s_i]=Y[f_i:s_i]
+			n_test_t[s_i:f_i+50]=Y[s_i:f_i+50]
+			f_i+=count
+			s_i+=count
+			ins+=ind
+			print(n_train_f)
+			print(f_i,s_i)
+		return n_train_f,n_train_t,n_test_f,n_test_t		
