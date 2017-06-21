ec9701822ee4edf8c273909cd80a9d9be4b4dc84
Aakash Dm <aakash.dokka@gmail.com>
2017-06-22 00:57:25 +0530

    a small change

--- ml/numc.py
+++ ml/numc.py
@@ -1,3 +1,4 @@
+from .GLM.lr import checkfit
 import numpy as np
 class nx:
 	"""a class for custom methods in numpy"""
@@ -19,30 +20,38 @@ class nx:
 		return b
 				
 def split_train_test(X,Y,per=20):
-		class_set=set(Y)
-		l=list(Y)
-		print("l",l)
-		count=l.count(list(class_set)[0])
-		print("coutn",count)
-		dim_1=int((per/100)*count)*len(list(class_set))
-		dim_2=X.shape[1]
-		print(dim_1,dim_2)
-		n_train_f=np.full([dim_1,dim_2],np.inf)
-		n_test_f=np.full([len(X)-dim_1,dim_2],np.inf)
-		n_train_t=np.full([dim_1,],np.inf)
-		n_test_t=np.full([len(X)-dim_1,],np.inf)	
-		f_i=0
-		ins=f_i
-		s_i=int((per/100)*count)
-		ind=s_i
-		for i in range(len(list(class_set))):
-			n_train_f[ins:ins+ind]=X[f_i:s_i]
-			n_test_f[s_i:f_i+50]=X[s_i:f_i+50]
-			n_train_t[f_i:s_i]=Y[f_i:s_i]
-			n_test_t[s_i:f_i+50]=Y[s_i:f_i+50]
-			f_i+=count
-			s_i+=count
-			ins+=ind
-			print(n_train_f)
-			print(f_i,s_i)
-		return n_train_f,n_train_t,n_test_f,n_test_t		
+		"""slpits training and testing data
+		X=feature vector
+		Y=target vector
+		per=percentage of data the training set should take.
+		Example:
+			from sklearn.datasets import load_iris
+			a=load_iris()
+			train_f,train_l,test_f,test_l=split_train_test(a.data,a.target)
+		"""
+		if checkfit(X,Y):
+			class_set=set(Y)
+			l=list(Y)
+			count=l.count(list(class_set)[0])
+			dim_1=int((per/100)*count)*len(list(class_set))
+			dim_2=X.shape[1]
+			n_train_f=np.full([dim_1,dim_2],np.inf)
+			n_test_f=np.full([len(X)-dim_1,dim_2],np.inf)
+			n_train_t=np.full([dim_1,],0)
+			n_test_t=np.full([len(X)-dim_1,],0)	
+			f_i=0
+			ins=f_i
+			s_i=int((per/100)*count)
+			ind=s_i
+			temp=0
+			for i in range(len(class_set)):
+				n_train_f[ins:ins+ind]=X[f_i:s_i]
+				n_test_f[temp:(count-ind)+temp]=X[s_i:f_i+count]
+				n_train_t[ins:ins+ind]=Y[f_i:s_i]
+				n_test_t[temp:(count-ind)+temp]=Y[s_i:f_i+count]
+				f_i+=count
+				s_i+=count
+				ins+=ind
+				temp+=count-ind
+			return n_train_f,n_train_t,n_test_f,n_test_t
+		
