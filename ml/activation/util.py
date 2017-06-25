2eb75886f6204b03b979768a6c5a9154b8b97535
Aakash Dm <aakash.dokka@gmail.com>
2017-06-26 01:38:29 +0530

    Update util.py

--- ml/activation/util.py
+++ ml/activation/util.py
@@ -10,7 +10,7 @@ def sigmoid(X):
 def tanh(X):
 	"""an activation function which outputs the value between (-1,1)"""
 	if isinstance(X,np.ndarray):
-		return (2.0/(1.0+np.exp(-2X)))-1
+		return (2.0/(1.0+np.exp(-(2*X))))-1
 	else:
 		X=np.array(X)
 		return tanh(X)
