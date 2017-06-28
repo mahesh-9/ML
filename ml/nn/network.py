996f8a5bf4a4b0a896dc97911fb3745f76595e81
Aakash Dm <aakash.dokka@gmail.com>
2017-06-29 03:08:25 +0530

    fixed few bugs

--- ml/nn/network.py
+++ ml/nn/network.py
@@ -1,5 +1,6 @@
 import numpy as np
-from ..activation import sigmoid
+from math import log
+from ..activation.util import sigmoid
 from ..numc import *
 from ..GLM.lr import checkfit,check_labels
 class brain:
@@ -9,20 +10,20 @@ class brain:
 		self.layers=[]
 		self.wpl=[]
 	def fit(self,X,Y,layers=None,neurons=None,one_hot=True):
-	""" Fits the training data:
+		""" Fits the training data:
 		Parameters:
 			X=training features(array like object)
 			Y=training labels(array like object)
 			layers=number of hidden layers(default=None)
 			neurons=number of neurons for each layer (list),default=None
 			one_hot=one hot vectors of the target values(training labels)
-	"""
+		"""
 		if checkfit(X,Y):
 			self.feat=X
 			self.m=len(X)
-			class_set=set(Y)
+			self.class_set=set(Y)
 			if one_hot:
-				Y=checklabels(Y)
+				Y=check_labels(Y)
 				self.target=Y
 			if layers != None:
 				if neurons==None:
@@ -31,13 +32,14 @@ class brain:
 					raise ValueError("parameter neurons should be of class list but given %s"%(type(neurons)))
 				if not isinstance(layers,int):
 					raise ValueError(" parameter layers should be of class int but given %s"%(type(layers))) 
-				self.totl=len(layers)
+				self.totl=layers
 				self.layers.append(X)
 				for i in range(self.totl):
 					l=[np.full([neurons[i],],0.0)]
 					self.layers.append(l)
 					if i==self.totl-1:
-						out_l=[np.full([len(list(class_set)),],0.0)]
+						out_l=[np.full([len(list(self.class_set)),],0.0)]
+						self.layers.append(out_l)
 				for i in range(1,(len(self.layers))):
 					w=np.random.random_sample((len(self.layers[i]),len(self.layers[i-1])+1))
 					self.wpl.append(w)
@@ -46,7 +48,7 @@ class brain:
 		inputs to the other hidden layers
 		Parameters:
 		it=layer number
-		prime:if True calculates the derivative of the activation function(sigmoid)(default:False)
+		prime:if True calculates the derivative (default:False)
 		"""
 		temp_f=self.feat
 		i=nx(temp_f)
@@ -62,34 +64,4 @@ class brain:
 			else:activations.append(sigmoid(s))
 		for i in range(len(self.layers[it])):
 			self.layers[it][i]=activations[i]
-	def cost(self):pass
-		
-		
-		
-		
-		
-		
-			
-					
-				
-			
-	
-			
-		
-		
-		
-		
-
-		
-		
-		
-					
-					
-				
-					
-				
-					
-					
-					
-
-		
+		return self
