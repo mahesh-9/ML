e0d9ef3870a04bb3e097f7352be9b5da9ba46ba4
Aakash Dm <aakash.dokka@gmail.com>
2017-06-30 01:24:23 +0530

    Update numc.py

--- ml/numc.py
+++ ml/numc.py
@@ -12,7 +12,9 @@ class nx:
 		which consists of an extra column (with a same value) to the original array object passed in __init__.
 		col_no=column number to add
 		val=value to be passed""" 
-		new_shape=self.ob.shape[0],self.ob.shape[1]+1
+		try: new_shape=self.ob.shape[0],self.ob.shape[1]+1
+		except IndexError:
+			new_shape=self.ob.shape[0],1
 		b=np.full(new_shape,np.inf)
 		for i in range(len(self.ob)):
 			temp=np.insert(self.ob[i],col_no,val)
