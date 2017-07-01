9aabc36a9fd4bc2808b69b58181c7841e999bc88
[umamahesh] <[umamaheshnov9@gmail.com]>
2017-07-01 23:03:02 +0530

    optimise

--- ml/GLM/LogisticRegr.py
+++ ml/GLM/LogisticRegr.py
@@ -7,13 +7,13 @@ class LogisticRegression():
 		#self.ran=0
 		return self
 	def hypo(self,it):
-		res=1/(1+e**(LR.hyp(self,it)))
+		res=1/(1+e**(-LR.hyp(self,it)))
 		return res		
 	def cost(self):
 		s=0
 		for _ in range(self.m):
 			s+=self.target[_]*log(self.hypo(_))+(1-self.target[_])*log(1-self.hypo(_))
-		r=(1/self.m)*s
+		r=(-1/self.m)*s
 		return r
 	def gd(self,rate=0.001,loops=100):
 		for k in range(loops):
