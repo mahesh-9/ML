12d2c8761d006bb4b4cd2b298fd35196b077648a
[umamahesh] <[umamaheshnov9@gmail.com]>
2017-07-10 22:36:18 +0530

    update SVM

--- ml/GLM/SVM.py
+++ ml/GLM/SVM.py
@@ -14,28 +14,30 @@ class SVC():
 			else:self.a=X
 			inst=nx(self.a)
 			self.feat=inst.add_col(col_no=0,val=1)
-			self.l =normalize(X)
+			self.l =normalize(self.feat)
 			self.target=Y
 			self.theta=np.zeros(self.feat.shape[1])
 			self.m=self.a.shape[0]
 			self.gd()
 	def f(self,x):
 		""" f is a function of features X and l are the landmarks which are equal to training set features """
-		x= normalize(x)
 		for i in range(len(self.m)):
-			f[i] = sim(self.x,self.l[i])
+			self.f[i] = sim(x,self.l[i])
 	
-	def sim(self,sigma=None):
-		S = np.exp(-(scipy.linalg.norm(self.x-self.l[i]))/2*(sigma**2))
+	def sim(self,sigma):
+		S = np.exp((scipy.linalg.norm(self.x-self.l[i]))/2*(sigma**2))
 		return S
 
 	def hypo(self,it):
-		return self.theta.dot(self.feat[it])
+		return self.theta.dot(self.f(it))
 
-	def cost(self,C=None):
+	def cost(self,C):
 		s =0 
 		for _ in range(self.m):
 			s+= C*((self.target[_])*log(self.hypo(_))+ (1-self.target[_])*log(1-self.hypo(_)))
+		
+		for j in range(self.m):
+			s+= (self.theta[j])**2/2
 		return s
 
 	def gd(self,rate=0.001,loops=100):
@@ -51,23 +53,25 @@ class SVC():
 	def predict(self,x):
 		self.gd()
 		x = np.array(x)
-		if self.theta.T.dot(f()) >= 0:return 1
-		else:return 0
+		if self.theta.T.dot(f()) >= 1:
+			return 1
+		else if np.dot(self.theta.T,self.f)<= -1:
+			return 0
 
-	def checkfit(X,Y):
-		if len(X)!=len(Y):
-			raise ValueError("The length of the features vector and target vector does not match")
-		elif len(X.shape)!=2:
-			raise ValueError("all the values in the feature vector do not have same dimensions")
-		else:return True
+def checkfit(X,Y):
+	if len(X)!=len(Y):
+		raise ValueError("The length of the features vector and target vector does not match")
+	elif len(X.shape)!=2:
+		raise ValueError("all the values in the feature vector do not have same dimensions")
+	else:return True
 	
-	def normalize(X):
-		"""This function normalizes the feature values:
-		new_x=(x-min(x))/(max(x)-min(x))"""
-		mi=np.min(X,axis=0)
-		ma=np.max(X,axis=0)
-		r=np.full(X.shape,np.inf)
-		for i in range(len(X)):
-			q=(X[i]-mi)/(ma-mi)
-			r[i]=q
-		return r	
+def normalize(X):
+	"""This function normalizes the feature values:
+	new_x=(x-min(x))/(max(x)-min(x))"""
+	mi=np.min(X,axis=0)
+	ma=np.max(X,axis=0)
+	r=np.full(X.shape,np.inf)
+	for i in range(len(X)):
+		q=(X[i]-mi)/(ma-mi)
+		r[i]=q
+	return r	
