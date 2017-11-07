f96345cb7b69eb308ce198e0a4de3c4fc533634c
aakash5897 <aakash.dokka@gmail.com>
2017-11-07 21:59:32 +0530

    modified

--- ml/losses.py
+++ ml/losses.py
@@ -1,60 +1,57 @@
 #TODO Not completed yet. 
 import numpy as np
-from math import log,e
+from math import log,e,exp
 from ml.activation.util import softmax,sigmoid
-class LOSS:
-	def __init__(self,feat=None,weights=None,hyp=None):
-		self.feat=feat
-		self.w=weights
-		self.hyp=hyp
+#class LOSS:
 
-	@staticmethod
-	def sigmoidal(self,X):
-		return sigmoid(normal(self,X))
+def sigmoidal(X):
+	return sigmoid(normal(self,X))
 
-	@staticmethod
-	def soft_max(self,X):
-		return softmax(self.normal(self,X))
 
-	@staticmethod
-	def mean_squared_loss(self,features,targets,hyp="Regression"):
-		self.f=features
-		self.hyp=HYP[hyp]
-		self.target=targets
-		self.no_train=len(self.feat)
-		su=0
-		for i in range(self.no_train):
-			res=(self.hyp(self.feat[i])-self.target[i])**2
-			su+=res
-		return np.mean(su)
+def soft_max(X):
+	"""an activation function which outputs the value between (0,1)"""
+	_X=np.full(X.shape,np.inf)
+	#for i in range(len(X)):
+	_X=np.exp(X)/(np.sum(np.exp(X),axis=0))
+	return _X
+	#@staticmethod
+def mean_squared_loss(features,targets,hyp="Regression"):
+	self.f=features
+	self.hyp=HYP[hyp]
+	self.target=targets
+	self.no_train=len(self.feat)
+	su=0
+	for i in range(self.no_train):
+		res=(self.hyp(self.feat[i])-self.target[i])**2
+		su+=res
+	return np.mean(su)
 
-	@staticmethod
-	def cross_entropy(self,targets,features,hyp="binary_class"):
-		self.feat=features
-		self.target=targets
-		self.hyp=HYP[hyp]
-		self.no_train=len(self.feat)
-		s=0
-		for _ in range(self.no_train):
-			s+=self.target[_]*log(self.hyp(self.feat[_]))+(1-self.target[_])*log(1-self.hypo(self.feat[_]))
-		r=(1/self.no_train)*s
-		return r
+	#@staticmethod
+def cross_entropy(self,targets,features,hyp="binary_class"):
+	self.feat=features
+	self.target=targets
+	self.hyp=HYP[hyp]
+	self.no_train=len(self.feat)
+	s=0
+	for _ in range(self.no_train):
+		s+=self.target[_]*log(self.hyp(self.feat[_]))+(1-self.target[_])*log(1-self.hypo(self.feat[_]))
+	r=(1/self.no_train)*s
+	return r
 
-	@staticmethod
-	def categorical_cross_entropy(self,feat,target,hyp="multiclass"):
-		self.target=target
-		self.k=self.target.shape[1]
-		self.feat=feat
-		self.no_train=len(self.feat)
-		self.hyp=HYP[hyp]
-		r = np.dot(self.target,np.log(self.hyp(self.feat)))
-		return r
+	#@staticmethod
+def categorical_cross_entropy(feat,target,hyp="multiclass",model="nn"):
+	target=target
+	#self.k=self.target.shape[1]
+	feat=feat
+	#self.no_train=len(self.feat)
+	#self.hyp=HYP[hyp]
+	r = np.dot(target,np.log(soft_max(feat)))
+	return r
 
-	@staticmethod
-	def normal(self,obj,X):
-		return np.matmul(obj.weights,X)
-
-HYP={"Regression":LOSS.normal,"binary_class":LOSS.sigmoidal,"multiclass":LOSS.soft_max}
+#@staticmethod
+def normal(X):
+	return np.matmul(obj.weights,X)
+#HYP={"Regression":LOSS.normal,"binary_class":LOSS.sigmoidal,"multiclass":LOSS.soft_max}
 			
 			
 			
