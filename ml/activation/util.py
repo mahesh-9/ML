bfb97d5ac55d5d13bf1a61b88d75b477eaa7327c
aakash5897 <aakash.dokka@gmail.com>
2017-11-12 12:05:46 +0530

    changed util

--- ml/activation/util.py
+++ ml/activation/util.py
@@ -3,22 +3,27 @@ import numpy as np
 def sigmoid(X):
 	"""an activation function which outputs the value between (0,1)"""
 	
-	if isinstance(X,np.ndarray):
-		return 1.0/(1.0+np.exp(-X))
-	else:
-		X=np.array(X)
-		return sigmoid(X)
-		#return 1.0/(1.0+np.exp(-X))
+	#if isinstance(X,np.ndarray):
+	result=[]
+	#for i in X:
+	
+#	res=1.0/(1.0+np.exp(-i))
 
+#		result.append(res)
+	#return np.asarray(result)
+	#else:	
+		#X=np.array(X)
+		#return sigmoid(X)
+	return 1.0/(1.0+np.exp(-X))
 def sigmoidDerivative(X):
 	"""  input : array of features
 	     output : Sigmoid derivative of the input  """
 
-	if isinstance(X,np.ndarray):
-		return sigmoid(X)*(1-sigmoid(X))
-	else:
-		X = np.array(X)
-		return sigmoidDerivative(X)
+	#if isinstance(X,np.ndarray):
+	return stable_sigmoid(X)*(1-stable_sigmoid(X))
+	#else:
+	#	X = np.array(X)
+	#	return sigmoidDerivative(X)
 
 def tanh(X):
 	"""an activation function which outputs the value between (-1,1)"""
@@ -46,9 +51,23 @@ def reluDerivative(X,ep=1e-1):
 	grads= 1.0*(X>0)
 	grads[grads==0]=1e-1
 	return grads
-		
-		
-"""for testing """	
-if __name__ == "__main__":
-	a = relu([-1,-2,0,1,3,4])
-	print(a)	
+def stable_sigmoid(X):
+	if isinstance(X,(list,np.ndarray)):
+		res=[]
+		for i in X:
+			if i>=0:
+				z=exp(-i)
+				res.append(z)
+			else:
+				z=exp(i)
+				res.append(z/(1+z))
+		return np.asarray(res)
+	else:
+		if X>=0:
+				z=exp(-X)
+				return 1/(1+z)
+		else:
+				z=exp(X)
+				return z/(1+z)
+			
+					
