8141ba7194e31011c944b117468d8ec82eddf69b
aakash5897 <aakash.dokka@gmail.com>
2019-01-06 20:24:14 +0530

    cleaned

--- ml/loading.py
+++ ml/loading.py
@@ -2,7 +2,7 @@ import sys
 import time
 def a(n):
 	r=chr(n)*1
-	for j in range(5):
+	for j in range(1):
 		s=""
 		for i in range(20):
 			s+=r
@@ -11,7 +11,7 @@ def a(n):
 			time.sleep(0.5)
 		sys.stdout.write("\n")
 
-if __name__=="__main__":
+#if __name__=="__main__":
 	#a(128187)
-	a(9209)
+	#a(9209)
 		
